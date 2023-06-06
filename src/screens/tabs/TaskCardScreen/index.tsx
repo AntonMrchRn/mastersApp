88490749c126f40f13ed648c64a07341639d8a05
Nikita Kovantsov <kovancov@gmail.com>
2023-06-06 10:32:27 +0300

    published

--- src/screens/tabs/TaskCardScreen/index.tsx
+++ src/screens/tabs/TaskCardScreen/index.tsx
@@ -1,19 +1,17 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { StackScreenProps } from '@react-navigation/stack';
 import { Badge, Card, TabControl, Text, Tips, useTheme } from 'rn-ui-kit';
-import { Variant } from 'rn-ui-kit/lib/typescript/components/Badge';
-import { TabItem } from 'rn-ui-kit/lib/typescript/components/TabControl';
 
-import { NightIcon } from '@/assets/icons/svg/screens/NightIcon';
-import { TaskCardDescription } from '@/components/TabScreens/TaskCard/TaskCardDescription';
 import { TaskCardHeader } from '@/components/TabScreens/TaskCard/TaskCardHeader';
 import {
   TaskSearchNavigationParamList,
   TaskSearchNavigatorScreenName,
 } from '@/types/navigation';
+
+import { useTaskCard } from './useTaskCard';
 
 import { styles } from './styles';
 
@@ -22,155 +20,8 @@ type TaskCardScreenProps = StackScreenProps<
   TaskSearchNavigatorScreenName.TaskCard
 >;
 export const TaskCardScreen: FC<TaskCardScreenProps> = ({ navigation }) => {
-  const [tab, setTab] = useState('Описание');
+  const { getBadges, tabs, onTabChange, getCurrentTab } = useTaskCard();
   const theme = useTheme();
-  const [status, setStatus] = useState<Status>('closed');
-  type Status =
-    | 'published'
-    | 'inProgress'
-    | 'workDelivery'
-    | 'done'
-    | 'paid'
-    | 'cancelled'
-    | 'closed';
-  type Badge = {
-    label: string;
-    icon: boolean | JSX.Element;
-    variant: Variant;
-    secondary?: boolean;
-  };
-  const getBadges = (): Badge[] => {
-    switch (status) {
-      case 'published':
-        return [
-          {
-            label: 'Опубликовано',
-            icon: false,
-            variant: 'basic',
-            secondary: true,
-          },
-          {
-            label: 'Срочно',
-            icon: true,
-            variant: 'secondary',
-            secondary: true,
-          },
-          {
-            label: 'Ночные работы',
-            icon: <NightIcon />,
-            variant: 'special',
-            secondary: true,
-          },
-        ];
-      case 'inProgress':
-        return [
-          {
-            label: 'В работе',
-            icon: false,
-            variant: 'accent',
-            secondary: true,
-          },
-          {
-            label: 'Срочно',
-            icon: true,
-            variant: 'secondary',
-            secondary: true,
-          },
-          {
-            label: 'Ночные работы',
-            icon: <NightIcon />,
-            variant: 'special',
-            secondary: true,
-          },
-        ];
-      case 'workDelivery':
-        return [
-          {
-            label: 'Сдача работ',
-            icon: false,
-            variant: 'warning',
-            secondary: true,
-          },
-        ];
-      case 'done':
-        return [
-          {
-            label: 'Ожидает оплаты',
-            icon: false,
-            variant: 'special',
-            secondary: true,
-          },
-        ];
-      case 'paid':
-        return [
-          {
-            label: 'Оплачено',
-            icon: false,
-            variant: 'success',
-            secondary: true,
-          },
-        ];
-      case 'cancelled':
-        return [
-          {
-            label: 'Отменено',
-            icon: false,
-            variant: 'danger',
-            secondary: true,
-          },
-        ];
-      case 'closed':
-        return [
-          {
-            label: 'Закрыто',
-            icon: false,
-            variant: 'basic',
-            secondary: true,
-          },
-        ];
-      default:
-        return [];
-    }
-  };
-
-  const tabs: TabItem[] = [
-    {
-      id: 0,
-      label: 'Описание',
-      icon: false,
-    },
-    {
-      id: 1,
-      label: 'Смета',
-      icon: false,
-    },
-    {
-      id: 2,
-      label: 'Комментарии',
-      icon: false,
-    },
-    {
-      id: 3,
-      label: 'Отчет',
-      icon: false,
-    },
-    {
-      id: 4,
-      label: 'История',
-      icon: false,
-    },
-  ];
-  const handleTabChange = (item: TabItem) => {
-    setTab(item.label);
-  };
-  const getCurrentTab = () => {
-    switch (tab) {
-      case 'Описание':
-        return <TaskCardDescription />;
-      default:
-        return <TaskCardDescription />;
-    }
-  };
   const goBack = () => {
     if (navigation.canGoBack()) {
       navigation.goBack();
@@ -208,7 +59,7 @@ export const TaskCardScreen: FC<TaskCardScreenProps> = ({ navigation }) => {
             text="Срок подачи сметы до 10 апреля 19:00"
             containerStyle={styles.tips}
           />
-          <TabControl data={tabs} initialId={0} onChange={handleTabChange} />
+          <TabControl data={tabs} initialId={0} onChange={onTabChange} />
         </View>
         <Card isShadow style={styles.card}>
           {getCurrentTab()}
---
+++ src/screens/tabs/TaskCardScreen/useTaskCard.tsx
@@ -0,0 +1,163 @@
+import React, { useState } from 'react';
+
+import { Variant } from 'rn-ui-kit/lib/typescript/components/Badge';
+import { TabItem } from 'rn-ui-kit/lib/typescript/components/TabControl';
+
+import { NightIcon } from '@/assets/icons/svg/screens/NightIcon';
+import { TaskCardDescription } from '@/components/TabScreens/TaskCard/TaskCardDescription';
+
+export type TaskCardStatus =
+  | 'published'
+  | 'inProgress'
+  | 'workDelivery'
+  | 'done'
+  | 'paid'
+  | 'cancelled'
+  | 'closed';
+export type TaskCardBadge = {
+  label: string;
+  icon: boolean | JSX.Element;
+  variant: Variant;
+  secondary?: boolean;
+};
+export const useTaskCard = () => {
+  const [status, setStatus] = useState<TaskCardStatus>('inProgress');
+  const [tab, setTab] = useState('Описание');
+  const getBadges = (): TaskCardBadge[] => {
+    switch (status) {
+      case 'published':
+        return [
+          {
+            label: 'Опубликовано',
+            icon: false,
+            variant: 'basic',
+            secondary: true,
+          },
+          {
+            label: 'Срочно',
+            icon: true,
+            variant: 'secondary',
+            secondary: true,
+          },
+          {
+            label: 'Ночные работы',
+            icon: <NightIcon />,
+            variant: 'special',
+            secondary: true,
+          },
+        ];
+      case 'inProgress':
+        return [
+          {
+            label: 'В работе',
+            icon: false,
+            variant: 'accent',
+            secondary: true,
+          },
+          {
+            label: 'Срочно',
+            icon: true,
+            variant: 'secondary',
+            secondary: true,
+          },
+          {
+            label: 'Ночные работы',
+            icon: <NightIcon />,
+            variant: 'special',
+            secondary: true,
+          },
+        ];
+      case 'workDelivery':
+        return [
+          {
+            label: 'Сдача работ',
+            icon: false,
+            variant: 'warning',
+            secondary: true,
+          },
+        ];
+      case 'done':
+        return [
+          {
+            label: 'Ожидает оплаты',
+            icon: false,
+            variant: 'special',
+            secondary: true,
+          },
+        ];
+      case 'paid':
+        return [
+          {
+            label: 'Оплачено',
+            icon: false,
+            variant: 'success',
+            secondary: true,
+          },
+        ];
+      case 'cancelled':
+        return [
+          {
+            label: 'Отменено',
+            icon: false,
+            variant: 'danger',
+            secondary: true,
+          },
+        ];
+      case 'closed':
+        return [
+          {
+            label: 'Закрыто',
+            icon: false,
+            variant: 'basic',
+            secondary: true,
+          },
+        ];
+      default:
+        return [];
+    }
+  };
+  const tabs: TabItem[] = [
+    {
+      id: 0,
+      label: 'Описание',
+      icon: false,
+    },
+    {
+      id: 1,
+      label: 'Смета',
+      icon: false,
+    },
+    {
+      id: 2,
+      label: 'Комментарии',
+      icon: false,
+    },
+    {
+      id: 3,
+      label: 'Отчет',
+      icon: false,
+    },
+    {
+      id: 4,
+      label: 'История',
+      icon: false,
+    },
+  ];
+  const getCurrentTab = () => {
+    switch (tab) {
+      case 'Описание':
+        return <TaskCardDescription />;
+      default:
+        return <TaskCardDescription />;
+    }
+  };
+  const onTabChange = (item: TabItem) => {
+    setTab(item.label);
+  };
+  return {
+    onTabChange,
+    getBadges,
+    tabs,
+    getCurrentTab,
+  };
+};
