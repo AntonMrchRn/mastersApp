6e0f536321e5cfeacef4dae9ca2c10892b94d897
Антон Марченко <marchenko_aa@apteka-aprel.ru>
2023-06-14 05:44:23 +0300

    коммит

--- src/components/TabScreens/TaskSearch/Card/index.tsx
+++ src/components/TabScreens/TaskSearch/Card/index.tsx
@@ -3,7 +3,8 @@ import { TouchableOpacity } from 'react-native';
 
 import { Card, Spacer, Text } from 'rn-ui-kit';
 
-export const CardTasks = ({ ID, navigation }) => {
+export const CardTasks = ({ name, description, navigation }, ...props) => {
+  console.log('props', props);
   return (
     <TouchableOpacity
       style={{
@@ -14,10 +15,9 @@ export const CardTasks = ({ ID, navigation }) => {
       onPress={() => navigation.navigate('TaskCard')}
     >
       <Card style={{ paddingHorizontal: 0 }}>
-        {/* <Text variant="title3">{test}</Text> */}
-        <Text variant="bodySRegular">{ID}</Text>
-        {/* <Text variant="captionRegular">{test}</Text> */}
-        {/* <Text variant="captionRegular">{test}</Text> */}
+        <Text variant="title3">{name}</Text>
+        <Text variant="bodySRegular">{description}</Text>
+        <Text variant="captionRegular">{description}</Text>
       </Card>
       <Spacer size="xs" separator="bottom" />
     </TouchableOpacity>
