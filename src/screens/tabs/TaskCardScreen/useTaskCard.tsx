3f532b7203debae4e2a31dc45f35479cb428fecc
Nikita Kovantsov <kovancov@gmail.com>
2023-06-28 09:47:10 +0300

    123

--- src/screens/tabs/TaskCardScreen/useTaskCard.tsx
+++ src/screens/tabs/TaskCardScreen/useTaskCard.tsx
@@ -42,8 +42,8 @@ export const useTaskCard = ({
   const { user } = useAppSelector(selectAuth);
 
   // const getTask = useGetTaskQuery('926');
-  const getTask = useGetTaskQuery('996');
-  // const getTask = useGetTaskQuery(taskId);
+  // const getTask = useGetTaskQuery('996');
+  const getTask = useGetTaskQuery(taskId);
 
   useEffect(() => {
     if (
