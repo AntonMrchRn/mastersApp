f6a7af3d681cd62566b3148920822823de6349d4
Nikita Kovantsov <kovancov@gmail.com>
2023-07-20 11:27:37 +0300

    MeasureItem

--- src/components/task/MeasureItem.tsx
+++ src/components/task/MeasureItem.tsx
@@ -8,7 +8,7 @@ import { CaretRightIcon } from '@/assets/icons/svg/estimate/CaretRightIcon';
 import { CubeLargeIcon } from '@/assets/icons/svg/estimate/CubeLargeIcon';
 import { Measure } from '@/types/task';
 
-import { EstimateMeasureBottomSheet } from './EstimateMeasureBottomSheet';
+import { EstimateMeasureBottomSheet } from '../TabScreens/EstimateAddMaterialScreen/EstimateMeasureBottomSheet';
 
 type MeasureItemProps = {
   measure: string;
