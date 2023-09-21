30e1a5730f3d686cd037da3180d523348b413f59
Nikita Kovantsov <kovancov@gmail.com>
2023-09-21 11:38:14 +0300

    123

--- src/services/notifications/iosPushPermission.tsx
+++ src/services/notifications/iosPushPermission.tsx
@@ -1,8 +1,10 @@
 import messaging from '@react-native-firebase/messaging';
 
 export const iosPushPermission = async () => {
-  const authorizationStatus = await messaging().requestPermission();
-  if (authorizationStatus) {
-    console.log('Permission status:', authorizationStatus);
+  const hasPermission = await messaging().hasPermission();
+  if (hasPermission !== messaging.AuthorizationStatus.DENIED) {
+    const authorizationStatus = await messaging().requestPermission();
+    return authorizationStatus;
   }
+  return hasPermission;
 };
