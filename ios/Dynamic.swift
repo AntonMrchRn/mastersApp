1eaac349440cc1ebe7d580fd04a4c40a56b8db3b
Nikita Kovantsov <kovancov@gmail.com>
2023-11-01 11:44:14 +0300

    fix/lottie pod

--- ios/Dynamic.swift
+++ ios/Dynamic.swift
@@ -5,16 +5,16 @@ import Lottie
 @objc class Dynamic: NSObject {
   
 
-  @objc func createAnimationView(rootView: UIView, lottieName: String) -> AnimationView {
+  @objc func createAnimationView(rootView: UIView, lottieName: String) -> LottieAnimationView {
     
-    let animationView = AnimationView(name: lottieName)
+    let animationView = LottieAnimationView(name: lottieName)
     animationView.frame = rootView.frame
     animationView.center = rootView.center
     animationView.backgroundColor = UIColor(red: 63.0/255, green: 81.0/255, blue: 181.0/255, alpha: 1.0)
     return animationView;
   }
 
-  @objc func play(animationView: AnimationView) {
+  @objc func play(animationView: LottieAnimationView) {
     animationView.play(
       completion: { (success) in
         RNSplashScreen.setAnimationFinished(true)
