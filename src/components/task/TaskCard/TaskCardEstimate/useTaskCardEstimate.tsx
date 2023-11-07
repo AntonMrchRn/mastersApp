8c314add8606bd9766278ee772e5c127cc6cab49
Oksana Sark <sarkitovao@gmail.com>
2023-11-07 16:34:31 +0300

    add invited curator flow

--- src/components/task/TaskCard/TaskCardEstimate/useTaskCardEstimate.tsx
+++ src/components/task/TaskCard/TaskCardEstimate/useTaskCardEstimate.tsx
@@ -19,6 +19,7 @@ import {
   setOfferID,
 } from '@/store/slices/tasks/actions';
 import { EstimateTab, RoleType, StatusType, TaskType } from '@/types/task';
+import { getInitServices } from '@/utils/getInitServices';
 
 export const useTaskCardEstimate = ({
   taskId,
@@ -184,32 +185,8 @@ export const useTaskCardEstimate = ({
 
   const onEditEstimate = () => {
     if (userOffer) {
-      const initServices: Service[] = userOffer.services.map(serv => {
-        const coordServices = services.find(ser => ser.name === serv.name);
-        const initMaterials: Material[] =
-          serv.materials?.map(mat => {
-            const coordMaterial = coordServices?.materials?.find(
-              m => m.name === mat.name,
-            );
-            return {
-              ...mat,
-              price: coordMaterial ? coordMaterial.price : mat.price,
-              canDelete: !coordMaterial,
-              localPrice: mat.price?.toString(),
-              count: coordMaterial ? coordMaterial.count : mat.count,
-              localCount: mat.count.toString(),
-            };
-          }) || [];
-        return {
-          ...serv,
-          canDelete: !coordServices,
-          price: coordServices ? coordServices.price : serv.price,
-          count: coordServices ? coordServices.count : serv.count,
-          localPrice: serv.price.toString(),
-          localCount: serv.count?.toString() || '',
-          materials: initMaterials,
-        };
-      });
+      const initServices: Service[] = getInitServices(userOffer, services);
+
       dispatch(setNewOfferServices(initServices));
       dispatch(setOfferComment(userOffer.comment));
       dispatch(setOfferID(userOffer.ID));
